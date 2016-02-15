a0f8fa6d41ee711168ae8de84c8c070b6c82b1eb
kimserey <kimserey.lam@gmail.com>
2016-02-15 23:31:12 +0000

    Added deedle

---
+++ London/Expenses.fsx
@@ -0,0 +1,10 @@
+﻿#I __SOURCE_DIRECTORY__
+#load "../packages/Deedle/Deedle.fsx"
+
+open System
+open Deedle
+
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+
+let expenses = 
+    Frame.ReadCsv("data/201511.csv")
