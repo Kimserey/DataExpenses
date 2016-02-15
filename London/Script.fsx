a21341087350c27d752a2bcc73a1b997d68f1f65
kimserey <kimserey.lam@gmail.com>
2016-02-15 22:53:49 +0000

    added gitignore

--- London/Script.fsx
+++ London/Script.fsx
@@ -1,8 +1,1 @@
-﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
-// for more guidance on F# programming.
-
-#load "Library1.fs"
-open London
-
-// Define your library scripting code here
-
+﻿
