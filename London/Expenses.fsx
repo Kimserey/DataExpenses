6b4570a8b04320fcd8b8a7b527b99228f0cee172
kimserey <kimserey.lam@gmail.com>
2016-02-16 02:05:41 +0000

    Added more code on the sum and average of expenses

--- London/Expenses.fsx
+++ London/Expenses.fsx
@@ -13,13 +13,10 @@ type Expense = {
     Amount: decimal
 }
 
-let dfs =
+let df =
     Directory.GetFiles(Environment.CurrentDirectory + "/data")
     |> Array.map (fun path -> Frame.ReadCsv(path, hasHeaders = false))
     |> Array.map (fun df -> df |> Frame.indexColsWith [ "Date"; "Title"; "Amount" ])
-
-let records =
-    dfs
     |> Array.map (fun df -> df.GetRows())
     |> Seq.collect (fun s -> s.Values)
     |> Seq.map (fun s -> s?Date, s?Title, s?Amount)
@@ -28,3 +25,40 @@ let records =
           Title = string title
           Amount = string amount |> decimal })
     |> Frame.ofRecords
+
+let ``sorted expenses`` =
+    df.Columns.[ ["Date"; "Title"; "Amount"] ]
+    |> Frame.getRows
+    |> Series.sortBy(fun s -> s?Amount)
+    |> Frame.ofRows
+
+let ``cumulated expenses grouped by title`` =
+    let x =
+        df.Columns.[ [ "Title"; "Amount" ] ]
+        |> Frame.groupRowsByString("Title")
+        |> Frame.getNumericCols
+        |> Series.mapValues (Stats.levelSum fst)
+    x?Amount
+    |> Series.observations
+    |> Seq.sortBy snd
+    |> Seq.toList
+
+``cumulated expenses grouped by title`` 
+|> List.iter (fun (title, amount) -> printfn "%50s %.2f" title amount)
+
+let ``average expenses grouped by title`` =
+    let x =
+        df.Columns.[ [ "Title"; "Amount" ] ]
+        |> Frame.groupRowsByString("Title")
+        |> Frame.getNumericCols
+        |> Series.mapValues (Stats.levelMean fst)
+    x?Amount
+    |> Series.observations
+    |> Seq.sortBy snd
+    |> Seq.toList
+
+``average expenses grouped by title``
+|> List.iter (fun (title, amount) -> printfn "%50s %.2f" title amount)
+
+
+//Make window count
