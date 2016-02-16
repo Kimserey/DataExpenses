f64f034b0fe14c1117e2b1f7ed72afbc3e2eeddf
kimserey <kimserey.lam@gmail.com>
2016-02-16 01:20:29 +0000

    merge all data in same dataset

--- London/Expenses.fsx
+++ London/Expenses.fsx
@@ -2,9 +2,29 @@
 #load "../packages/Deedle/Deedle.fsx"
 
 open System
+open System.IO
 open Deedle
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let expenses = 
-    Frame.ReadCsv("data/201511.csv")
+type Expense = {
+    Date: DateTime
+    Title: string
+    Amount: decimal
+}
+
+let dfs =
+    Directory.GetFiles(Environment.CurrentDirectory + "/data")
+    |> Array.map (fun path -> Frame.ReadCsv(path, hasHeaders = false))
+    |> Array.map (fun df -> df |> Frame.indexColsWith [ "Date"; "Title"; "Amount" ])
+
+let records =
+    dfs
+    |> Array.map (fun df -> df.GetRows())
+    |> Seq.collect (fun s -> s.Values)
+    |> Seq.map (fun s -> s?Date, s?Title, s?Amount)
+    |> Seq.map (fun (date, title, amount) -> 
+        { Date = string date |> DateTime.Parse
+          Title = string title
+          Amount = string amount |> decimal })
+    |> Frame.ofRecords
