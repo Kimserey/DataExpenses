cfaeb61f6321adccc87aa1d6c97d31118d15392e
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-05 12:01:27 +0100

    Changed to ui next template

--- London.Web/Site.fs
+++ London.Web/Site.fs
@@ -3,21 +3,12 @@ namespace London.Web
 open WebSharper
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Html
-open WebSharper.Sitelets
+open WebSharper.UI.Next.Server
 
 module Site =
+    
+    type MainTemplate = Templating.Template<"Main.html">
 
-    type MainHtml = {
-        Title : string
-        Body : Doc
-    }
- 
-    let template =
-        Content.Template<MainHtml>("~/Main.html")
-            .With("title", fun x -> x.Title)
-            .With("body", fun x -> x.Body)
-
-    let page =
-        Content.WithTemplate template { Title = " Data expenses"; Body = client <@ Client.page @> }
+    let page = Content.Page(MainTemplate.Doc(title = "Data expenses", body = [ client <@ Client.page @> ]))
 
     let app = Application.SinglePage(fun _ -> page)
