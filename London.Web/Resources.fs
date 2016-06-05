85c1f8df7543f00d49eb77b8e94c176cb439b3db
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-05 11:29:18 +0100

    Added style.css

---
+++ London.Web/Resources.fs
@@ -0,0 +1,13 @@
+﻿namespace London.Web
+
+open WebSharper
+open WebSharper.Resources
+
+module Resources =
+    
+    type Style() =
+        inherit BaseResource("style.css")
+
+    [<assembly:System.Web.UI.WebResource("style.css", "text/css"); 
+      assembly:Require(typeof<Style>)>]
+    do()
