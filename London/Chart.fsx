91d9d00f389993fe4fd2fc07989b02b423c51f1c
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-26 17:39:57 +0100

    Removed sample

--- London/Chart.fsx
+++ London/Chart.fsx
@@ -22,24 +22,3 @@ df
 df
 |> ExpenseDataFrame.GetSmoothExpenses Category.Supermarket "Date"
 |> List.iter (fun e -> printfn "%10s %5.2f" (e.Date.ToShortDateString()) e.Amount)
-
-
-type Test = {
-    Date: DateTime
-    Label: string
-    Amount: float
-} with
-    static member Create(date, label, amount) =
-        { Date = date; Label = label; Amount = amount }
-    override x.ToString() =
-        sprintf "%s - %10s - %.2f" ( x.Date.ToShortDateString()) x.Label x.Amount
-
-let testDf =
-    [ Test.Create(new DateTime(2016, 2, 1), "Supermarket", 15.)
-      Test.Create(new DateTime(2016, 2, 10), "Supermarket", 25.)
-      Test.Create(new DateTime(2016, 2, 16), "Clothes", 15.)
-      Test.Create(new DateTime(2016, 3, 10), "Supermarket", 65.) ]
-    |> Frame.ofRecords
-
-testDf
-|> Frame.rows
