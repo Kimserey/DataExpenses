06e437eecb5f347c1f2eb6fd34cc18a9e264d941
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-06-30 23:40:30 +0100

    added link to SO question for reference

--- London.Web/Resources/Postscript.js
+++ London.Web/Resources/Postscript.js
@@ -1,2 +1,3 @@
 ﻿// Fix Electron not seeing JQuery
+// http://stackoverflow.com/questions/32621988/electron-jquery-is-not-defined
 if (window.module) module = window.module;
--- London.Web/Resources/Prescript.js
+++ London.Web/Resources/Prescript.js
@@ -1,2 +1,3 @@
 ﻿// Fix Electron not seeing JQuery
+// http://stackoverflow.com/questions/32621988/electron-jquery-is-not-defined
 if (typeof module === 'object') { window.module = module; module = undefined; }
