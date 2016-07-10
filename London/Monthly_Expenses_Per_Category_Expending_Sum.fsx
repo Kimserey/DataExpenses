078fb19bb772f51db2ba5129d49172c218d495ab
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-07-10 10:54:53 +0100

    added expanded sum for every months per category

---
+++ London/Monthly_Expenses_Per_Category_Expending_Sum.fsx
@@ -0,0 +1,30 @@
+﻿#load "_references.fsx"
+open _references
+open System
+open Deedle
+open London.Core
+
+(*
+    Experimentation
+*)
+
+// TODO - Join on full date range
+
+df
+|> Frame.pivotTable
+    (fun _ c ->  c.GetAs<DateTime>("Date").Date)
+    (fun _ c -> c.GetAs<string>("Category"))
+    (fun frame -> frame |> Stats.sum |> Series.get "Amount")
+|> Frame.fillMissingWith 0.
+|> Frame.sortRowsByKey
+|> Frame.groupRowsUsing (fun (d: DateTime) _ -> d.Month, d.Year)
+|> Frame.nest
+|> Series.mapValues (Frame.getNumericCols >> (Series.mapValues Stats.expandingSum) >> Frame.ofColumns)
+|> Frame.unnest
+
+(*
+    Call from Shared library
+*)
+df
+|> ExpenseDataFrame.GetExpendingMean Category.Supermarket
+|> Seq.iter(fun x -> printfn "%A" x)
