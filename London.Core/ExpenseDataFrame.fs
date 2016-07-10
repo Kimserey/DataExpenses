416abe23226b66c72d1aaa95ae69f9d76174a392
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-07-10 12:03:43 +0100

    added to dataframe function to compute the monthly expanding sum for each day per dataframe.

--- London.Core/ExpenseDataFrame.fs
+++ London.Core/ExpenseDataFrame.fs
@@ -358,6 +358,47 @@ type ExpenseDataFrame = {
         |> Seq.map snd
         |> Seq.toList
 
+    static member GetMonthlyExpandingSumForEachDayPerCategory exp =
+        let allDates =
+            let dates =
+                exp
+                |> Frame.getCol "Date"
+                |> Series.mapValues DateTime.Parse
+    
+            // Here I know that the dates will never be None
+            // otherwise something is seriously wrong in the data
+            let min = Stats.min dates |> Option.get
+            let max = Stats.max dates |> Option.get
+    
+            [0..(max - min).Days]
+            |> List.map (float >> min.AddDays)
+
+        exp
+        |> Frame.pivotTable
+            (fun _ c ->  c.GetAs<DateTime>("Date").Date)
+            (fun _ c -> c.GetAs<string>("Category"))
+            (fun frame -> frame |> Stats.sum |> Series.get "Amount")
+        |> Frame.realignRows allDates
+        |> Frame.fillMissingWith 0.
+        |> Frame.sortRowsByKey
+        |> Frame.groupRowsUsing (fun (d: DateTime) _ -> d.Month, d.Year)
+        |> Frame.nest
+        |> Series.mapValues (Frame.getNumericCols >> (Series.mapValues Stats.expandingSum) >> Frame.ofColumns)
+        |> Series.observations
+        |> Seq.map (fun ((month, year), frame) ->
+            Month (monthToString month, month),
+            Year year,
+            frame 
+            |> Frame.getCols
+            |> Series.observations
+            |> Seq.map (fun (category, series) ->
+                Title category,
+                series
+                |> Series.observations
+                |> Seq.toList)
+            |> Seq.toList)
+        |> Seq.toList
+
 module Dataframe =
     open System.IO
 
--- London/Monthly_ExpandingSum_ForEachDay_PerCategory.fsx
+++ London/Monthly_ExpandingSum_ForEachDay_PerCategory.fsx
@@ -8,13 +8,26 @@ open London.Core
     Experimentation
 *)
 
-// TODO - Join on full date range
+let allDates =
+    let dates =
+        df
+        |> Frame.getCol "Date"
+        |> Series.mapValues DateTime.Parse
+    
+    // Here I know that the dates will never be None
+    // otherwise something is seriously wrong in the data
+    let min = Stats.min dates |> Option.get
+    let max = Stats.max dates |> Option.get
+    
+    [0..(max - min).Days]
+    |> List.map (float >> min.AddDays)
 
 df
 |> Frame.pivotTable
     (fun _ c ->  c.GetAs<DateTime>("Date").Date)
     (fun _ c -> c.GetAs<string>("Category"))
     (fun frame -> frame |> Stats.sum |> Series.get "Amount")
+|> Frame.realignRows allDates
 |> Frame.fillMissingWith 0.
 |> Frame.sortRowsByKey
 |> Frame.groupRowsUsing (fun (d: DateTime) _ -> d.Month, d.Year)
@@ -26,5 +39,5 @@ df
     Call from Shared library
 *)
 df
-|> ExpenseDataFrame.GetExpendingMean Category.Supermarket
+|> ExpenseDataFrame.GetMonthlyExpandingSumForEachDayPerCategory
 |> Seq.iter(fun x -> printfn "%A" x)
