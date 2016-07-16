1685d476ba27f7ff8c2f341c105932bd1ef5fec6
Kimserey <kimserey.lam@housing-insight.co.uk>
2016-07-16 21:07:29 +0100

    Added suave ref. Added an index.html in London to directly display data in graph using suave and hightcharts

--- London/Gradient_Descent.fsx
+++ London/Gradient_Descent.fsx
@@ -8,36 +8,29 @@ open London.Core
     Experimentation
 *)
 
-let frame =
+let data =
     df
-    |> Frame.pivotTable
-        (fun _ c ->  c.GetAs<DateTime>("Date").Day)
-        (fun _ c -> 
-            let d = c.GetAs<DateTime>("Date")
-            d.Month, d.Year)
-        (fun frame -> frame |> Stats.sum |> Series.get "Amount")
-    |> Frame.realignRows [1..31]
-    |> Frame.fillMissingWith 0.
+    |> Frame.filterRowValues (fun c -> 
+        let date = c.GetAs<DateTime>("Date")
+        date.Month = 6 && date.Year = 2016 && c.GetAs<string>("Category") = "Supermarket")
+    |> Frame.groupRowsBy "Date"
+    |> Frame.sortRowsByKey
     |> Frame.getNumericCols
-    |> Series.mapValues Stats.expandingSum
-    |> Frame.ofColumns
+    |> Series.get "Amount"
+    |> Stats.levelSum fst
+    |> Stats.expandingSum
+    |> Series.observations
+    |> Seq.map (fun ((date: DateTime), value) ->
+        float date.Day, value)
+    |> Seq.toList
 
 // Apply linear regression to July 2016 to predict expenses of the monht
 // Least square cost cost function = 1/2N * Sum (estimate - real value) square
 // Estimate = Theta1 * x + Theta0
-let alpha = 0.01
-let epsilon = 0.01
-let data =
-    [ (1., 143.)
-      (2., 243.)
-      (3., 263.)
-      (4., 303.)
-      (5., 343.)
-      (6., 350.)
-      (7., 351.)
-      (8., 373.)
-      (9., 403.)
-      (10., 401.) ]
+
+// learning rate alpha
+let alpha = 0.1
+
 let m =
     data.Length
 
@@ -63,7 +56,7 @@ let next thethas =
                 |> List.map (fun (x, y) -> thetha0 + thetha1 * x - y)
                 |> List.sum
 
-            thetha0 - alpha * (1./float m) * sum
+            thetha0 - (alpha * (1./float m) * sum)
 
         let thetha1' =
             let sum =
@@ -72,30 +65,61 @@ let next thethas =
                 |> List.map (fun (x, y) -> (thetha0 + thetha1 * x - y) * x)
                 |> List.sum
         
-            thetha1 - alpha * (1./float m) * sum
+            thetha1 - (alpha * (1./float m) * sum)
 
         [thetha0'; thetha1']
-    | _ -> thethas
+    | _ -> failwith "Could not compute next thethas, thethas are not in correct format."
 
 let compute iterations =
     [0..iterations]
     |> List.scan (fun thethas _ -> next thethas) [0.; 0.]
-
+    
 let thethas = 
-    compute 5000
+    compute 3
+    |> List.map (fun x -> printfn "%A" x; x)
     |> List.last
 
-let cost =
-    costFunc thethas
+let cost = costFunc thethas
 
-printfn "%A  - cost: %.4f%%" thethas (cost * 100.)
+printfn "%A  / cost: %.4f%%" thethas (cost * 100.)
 
 printfn "%A" ([1..10] |> List.map (fun i -> thethas.[0] + thethas.[1] * float i))
 
 (*
     Call from Shared library
 *)
-df
-|> ExpenseDataFrame.FromFrame
-|> ExpenseDataFrame.GetDailyExpandingSumPerMonth
-|> Seq.iter(fun x -> printfn "%A" x)
+//df
+//|> ExpenseDataFrame.FromFrame
+//|> ExpenseDataFrame.GetDailyExpandingSumPerMonth
+//|> Seq.iter(fun x -> printfn "%A" x)
+
+#I __SOURCE_DIRECTORY__
+#r "../packages/Suave/lib/net40/Suave.dll"
+#r "../packages/Newtonsoft.Json/lib/net40/Newtonsoft.Json.dll"
+
+open Suave
+open Suave.Files
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+open Suave.Writers
+open Newtonsoft.Json
+open Newtonsoft.Json.Serialization
+
+let JSON v =
+  OK (JsonConvert.SerializeObject(v, new JsonSerializerSettings(ContractResolver = new CamelCasePropertyNamesContractResolver())))
+  >=> setMimeType "application/json; charset=utf-8"
+  >=> setHeader "Access-Control-Allow-Origin" "*"
+  >=> setHeader "Access-Control-Allow-Headers" "content-type"
+
+type Data = {
+    X: float
+    Y: float
+} with
+    static member FromTuple (x, y) = { X = x; Y = y }
+
+let app = 
+    GET >=> choose
+        [ path "/data" >=> JSON [ data |> List.map Data.FromTuple ] ]
+
+startWebServer defaultConfig app
